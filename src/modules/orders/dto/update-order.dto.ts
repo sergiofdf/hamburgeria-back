be628337d47dad2db9636ba5aedc37a489b13bbb
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2023-10-29 23:55:07 -0300

    change total property to optional in updateOrderDto

--- src/modules/orders/dto/update-order.dto.ts
+++ src/modules/orders/dto/update-order.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { OrderStatus } from '@prisma/client';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateOrderDto {
   @ApiProperty()
   @IsNotEmpty()
   status: OrderStatus;
   @ApiProperty()
-  @IsNotEmpty()
+  @IsOptional()
   total: number;
 }
